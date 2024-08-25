bae7b3e068b8a77f15c34010e4a9c1a7c01a58c1
devdeepak06 <k.deepak2217@gmail.com>
2024-08-25 13:55:47 +0530

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1>Welcome to elib client app</h1>
+    <>
+      <h1>Welcome to elib client app</h1>
+    </>
   );
 }
