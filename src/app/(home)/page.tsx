8af22098a9dc2b45e33a41979c0642b3ed2c9eb4
devdeepak06 <k.deepak2217@gmail.com>
2024-08-25 16:53:27 +0530

    create bookcard

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
 import Banner from "@/app/(home)/components/Banner";
+import Image from "next/image";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
+// export const dynamic = 'force-dynamic';
+
 export default async function Home() {
-  const response = await fetch(`${process.env.BACKEND_URL}/books`);
-  if (!response.ok) {
-    throw new Error("An error occured while fetching the books");
-  }
-  const books = await response.json();
-  console.log("books", books);
   return (
     <>
       <Banner />
-      <BookList />
+      <Suspense fallback={<Loading />}>
+        <BookList />
+      </Suspense>
     </>
   );
 }
