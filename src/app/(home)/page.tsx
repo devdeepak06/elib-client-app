7d28357c94641a826949a3293fbf4ddc3cff4c59
devdeepak06 <k.deepak2217@gmail.com>
2024-08-25 15:03:04 +0530

    reorganize components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Banner from "@/components/Banner";
+import Banner from "@/app/(home)/components/Banner";
 export default function Home() {
   return (
     <>
